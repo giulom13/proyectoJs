212038fcf4862469d15b7abdd091c22ab60558b5
Giulianno Magistocchi <gmagistocchi.13@gmail.com>
2024-05-27 18:01:54 -0300

    subir a github

---
+++ main.js
@@ -0,0 +1,83 @@
+let username = "Giulo";
+let password = "palabra";
+
+let usernameInput = prompt("Ingrese su nombre de usuario");
+let passwordInput = prompt("Ingrese su contraseña");
+
+while ((username != usernameInput) || password != passwordInput) {
+    alert("Nombre de usuario y/o contraseña incorrecto");
+    usernameInput = prompt("Ingrese nuevamente su nombre de usuario");
+    passwordInput = prompt("Ingrese nuevamente su contraseña");
+}
+
+alert("Juguemos! Piedra, papel, tijera, lagarto, Spock! :)");
+alert("Las reglas son muy sencillas");
+alert("Las tijeras cortan el papel");
+alert("El papel cubre la piedra");
+alert("La piedra aplasta el lagarto.");
+alert("El lagarto envenena a Spock.");
+alert("Spock aplasta las tijeras");
+alert("Las tijeras decapitan el lagarto.");
+alert("El lagarto se come el papel.");
+alert("El papel refuta a Spock.");
+alert("Spock vaporiza la piedra.");
+alert("La piedra aplasta a las tijeras.");
+
+alert("Estas listo?");
+
+let jugadorUno = prompt("Ingrese el nombre del jugador 1");
+let jugadorDos = prompt("Ingrese el nombre del jugador 2");
+let rondas = Number(prompt("Ingrese la cantidad de rondas que desea jugar"));
+
+while (isNaN(rondas)) {
+    rondas = Number(prompt("Puede ponerlo en numeros por favor?"));
+}
+
+function juegoJs() {
+    let contadorUno = 0;
+    let contadorDos = 0;
+
+    alert("Comencemos!");
+    for (let i = 1; i <= rondas; i++) {
+        let jugadaUno = prompt("Jugada n° " + i + " " + jugadorUno + " " + "Elije tu juego! Piedra, papel, tijera, lagarto, Spock!").toLowerCase();
+        let jugadaDos = prompt("Jugada n° " + i + " " + jugadorDos + " " + "Elije tu juego! Piedra, papel, tijera, lagarto, Spock!").toLowerCase();
+        
+        while ((jugadaUno != "piedra" && jugadaUno != "papel" && jugadaUno != "tijera" && jugadaUno != "lagarto" && jugadaUno != "spock") || (jugadaDos != "piedra" && jugadaDos != "papel" && jugadaDos != "tijera" && jugadaDos != "lagarto" && jugadaDos != "spock")) {
+            alert("No es tan dificil muchachos. EN PALABRAS Y COMO ESTA ESCRITO!! Piedra, papel, tijera, lagarto, Spock");
+            jugadaUno = prompt(jugadorUno + ": Piedra, papel, tijera, lagarto, Spock");
+            jugadaDos = prompt(jugadorDos + ": Piedra, papel, tijera, lagarto, Spock");
+        }
+
+        if (jugadaUno === jugadaDos) {
+            alert("Empate! Vuelve a tirar");
+            i--;
+        } else if ((jugadaUno === "piedra" && (jugadaDos === "tijera" || jugadaDos === "lagarto")) || (jugadaUno === "papel" && (jugadaDos === "piedra" || jugadaDos === "spock")) || (jugadaUno === "tijera" && (jugadaDos === "papel" || jugadaDos === "lagarto")) || (jugadaUno === "lagarto" && (jugadaDos === "spock" || jugadaDos === "papel")) || (jugadaUno === "spock" && (jugadaDos === "tijera" || jugadaDos === "piedra"))) {
+            contadorUno++;
+            alert(jugadorUno + ": Felicitaciones! Has ganado la ronda " + i);
+        } else {
+            contadorDos++;
+            alert(jugadorDos + ": Felicitaciones! Has ganado la ronda " + i);
+        }
+    }
+
+    alert("Juego terminado! Vamos a ver quien gano");
+    if (contadorUno > contadorDos) {
+        alert("Gano " + jugadorUno + "! El puntaje fue de " + contadorUno + " contra " + contadorDos + " de " + jugadorDos);
+    } else if (contadorUno < contadorDos) {
+        alert("Gano " + jugadorDos + "! El puntaje fue de " + contadorDos + " contra " + contadorUno + " de " + jugadorUno);
+    } else {
+        alert("Empate! el puntaje fue " + contadorUno + " a " + contadorDos);
+    }
+}
+
+juegoJs();
+
+// bucle para confirmar jugar de nuevo
+while (confirm("¿Revancha?")) {
+    juegoJs();
+}
+
+alert("Gracias por jugar!");
+
+
+
